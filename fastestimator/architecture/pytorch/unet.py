d2fad3167d15aacb80853b1fc2fd2f91e0da9269
JP Hong <junpyo.hong@ge.com>
2020-02-14 17:33:35 -0800

    WIP in pytorch UNET

--- fastestimator/architecture/pytorch/unet.py
+++ fastestimator/architecture/pytorch/unet.py
@@ -12,6 +12,67 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
+from typing import Tuple, Iterable
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as fn
+
+
+class UNet(nn.Module):
+    def __init__(self,
+                 input_shape: Tuple[int, int, int] = (3, 128, 128),
+                 dropout: float = 0.5,
+                 nchannels: Iterable[int] = (64, 128, 256, 512, 1024),
+                 nclasses: int = 1,
+                 bn: str = None,
+                 activation: str = 'relu',
+                 upsampling: str = 'bilinear',
+                 dilation_rates: Iterable[int] = (1, 1, 1, 1, 1),
+                 residual: str = None):
+        super().__init__()
+        self.input_shape = input_shape
+        self.dropout = dropout
+        self.nchannels = nchannels
+
+    def forward(self, x):
+        return x
+
+
+class UNetDownSampleBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size):
+        super().__init__()
+        padding = int(kernel_size // 2)
+        self.layers = nn.Sequential([
+            nn.Conv2d(in_channels, out_channels, kernel_size, padding=padding),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size, padding=padding),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2)
+        ])
+
+    def forward(self, x):
+        return self.layers(x)
+
+
+class UNetUpSampleBlock(nn.Module):
+    def __init__(self, in_channels, mid_channels, out_channels, kernel_size):
+        super().__init__()
+        padding = int(kernel_size // 2)
+        self.layers = nn.Sequential([
+            nn.Conv2d(in_channels, mid_channels, kernel_size, padding=padding),
+            nn.BatchNorm2d(mid_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(mid_channels, mid_channels, kernel_size, padding=padding),
+            nn.BatchNorm2d(mid_channels),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(mid_channels,
+                               out_channels,
+                               kernel_size=2,
+                               stride=2)
+        ])
+
+    def forward(self, x):
+        return self.layers(x)
